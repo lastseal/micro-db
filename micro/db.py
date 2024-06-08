76a5a85483751dd5319e667e8948b240c5f2f461
Rodrigo Arriaza <rarriaza@lastseal.com>
2024-06-07 21:19:36 -0400

    Update db.py

--- micro/db.py
+++ micro/db.py
@@ -1,2 +1,16 @@
 # -*- coding: utf-8 -*
 
+import pandas as pd
+import psycopg2
+import psycopg2.extras
+import logging
+import os
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "database")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+##
+#
